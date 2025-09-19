e2bde84a4bde63f3736a6c70fd685c7e3f85c5d1
muhdshahan <shahuuraff@gmail.com>
2025-09-19 19:20:50 +0530

    Change unit testing

--- tests/test_ml_model.py
+++ tests/test_ml_model.py
@@ -8,25 +8,25 @@ def text1():
     """ Fixed text for reusing using fixture """
     return "Hello friend, have a nice day!"
 
-def test_toxicity_false():
+def test_toxicity_false(text1):
     """ Testing if text is clean """
     assert is_toxic(text1, 0.5) is False
 
-def test_mask_abusive_false():
+def test_mask_abusive_false(text1):
     """ Testing clean text = original text """
     masked = mask_abusive_words(text1, 0.5)
-    assert masked == text1()
+    assert masked == text1
 
 @pytest.fixture
 def text2():
     """ Fixed text for reusing using fixture """
     return "You are an idiot"
 
-def test_toxicity_true():
+def test_toxicity_true(text2):
     """ Testing if text is toxic """
     assert is_toxic(text2, 0.5) is True
 
-def test_mask_abusive():
+def test_mask_abusive(text2):
     """ Testing cleaned text != original text """
     masked = mask_abusive_words(text2, 0.5)
     assert "idiot" not in masked
